5b03a158b5a71d9075a456cd004cb9c839dc06f7
Technyen <penghe201211@gmail.com>
2023-10-19 14:51:42 -0300

    Add project files.

---
+++ OAuthFlows/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OAuthFlows.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
